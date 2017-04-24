4260fedc048fcc55277190e5e7f6e33625603aed
Nathan Kluth <nlkluth@gmail.com>
2017-04-24 07:54:41 -0600

    initial setup

---
+++ src/DecisionTreeLearning.java
@@ -0,0 +1,69 @@
+import java.util.HashMap;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.apache.spark.mllib.tree.DecisionTree;
+import org.apache.spark.storage.StorageLevel;
+
+import scala.Tuple2;
+import scala.collection.immutable.Map;
+
+public class DecisionTreeLearning {
+	public static void main(String[] args) {
+		// Create Java spark context
+		SparkConf conf = new SparkConf().setAppName("Decision Tree");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		JavaRDD<LabeledPoint> training = sc.textFile(args[0]).persist(
+				StorageLevel.MEMORY_ONLY_SER()).map(new Function<String, LabeledPoint>() {
+					
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public LabeledPoint call(String arg0) throws Exception {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
+		
+		JavaRDD<LabeledPoint> test = sc.textFile(args[0]).persist(
+				StorageLevel.MEMORY_ONLY_SER()).map(new Function<String, LabeledPoint>() {
+					
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public LabeledPoint call(String arg0) throws Exception {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
+		
+		Integer numClasses = 2;
+		HashMap<Object, Object> categoricalFeaturesInfo = new HashMap<>();
+		Map <Object, Object> myMap = RandomForestLearning.toScalaMap(categoricalFeaturesInfo);
+		String impurity = "gini";
+		Integer maxDepth = 5;
+		Integer maxBins = 32;
+
+		final org.apache.spark.mllib.tree.model.DecisionTreeModel model = DecisionTree.trainClassifier(training.rdd(), numClasses,
+				  myMap, impurity, maxDepth, maxBins);
+		
+		JavaPairRDD<Double, Double> predictionAndLabel =
+			test.mapToPair(new PairFunction<LabeledPoint, Double, Double>() {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public Tuple2<Double, Double> call(LabeledPoint p) {
+					return new Tuple2<>(model.predict(p.features()), p.label());
+			  	}
+			});				
+		
+		predictionAndLabel.coalesce(1).saveAsTextFile("hdfs://denver:43401/output");
+		sc.close();
+	}
+}
